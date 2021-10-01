14eb170b07beac7196fff3dd1248f27317f28fcc
simon <simon.lardinois@hotmail.com>
2021-10-01 16:35:18 +0200

    first commit

---
+++ app/src/main/java/eu/nviso/guardsquare/Test.kt
@@ -0,0 +1,14 @@
+package eu.nviso.guardsquare
+
+import android.util.Log
+
+class Test {
+    init {
+        Log.d("Test", "test")
+    }
+
+    fun log() {
+
+        Log.d("Test", "test")
+    }
+}
---
+++ app/src/main/java/eu/nviso/guardsquare/Testa.kt
@@ -0,0 +1,48 @@
+package eu.nviso.guardsquare
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import eu.nviso.guardsquare.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class Testa : Fragment() {
+    val API_KEY="AAAAAAAAAAA"
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        Log.d("test", "test")
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("test", "test")
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
